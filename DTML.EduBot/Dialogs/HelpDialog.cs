cabbaaba004f32af9832878f432cfa65d0c27815
Kylie Strawn <kypaschk@microsoft.com>
2017-07-25 09:36:35 -0700

    Add Help Dialog

---
+++ DTML.EduBot/Dialogs/HelpDialog.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DTML.EduBot.Dialogs
+{
+    public partial class HelpDialog : QnaLuisDialog<object>
+    {
+        [LuisIntent("Help")]
+        public async Task HandleHelpIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("");
+        }
+
+        [LuisIntent("Repeat")]
+        public async Task HandleRepeatIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("");
+        }
+    }
+}
