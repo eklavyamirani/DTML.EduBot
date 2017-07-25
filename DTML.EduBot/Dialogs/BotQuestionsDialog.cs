569f80443f8f14652c6bcaa90a60f1b120b838ff
Cynthia Shlezar <t-cyshle@microsoft.com>
2017-07-25 09:25:54 -0700

    added Dialog classes for Date, Navigate, Meaning, and BotQuestions

---
+++ DTML.EduBot/Dialogs/BotQuestionsDialog.cs
@@ -0,0 +1,24 @@
+﻿namespace DTML.EduBot.Dialogs
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using DTML.EduBot.Common;
+    using DTML.EduBot.Qna;
+    using Microsoft.Azure;
+    using Attributes;
+
+    [PreConfiguredLuisModel]
+    [PreConfiguredQnaModel]
+    [Serializable]
+    public partial class BotQuestionsDialog : QnaLuisDialog<object>
+    {
+        [LuisIntent("BotQuestions")]
+        public async Task HandleBotQuestionsIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("");
+        }
+    }
+}
